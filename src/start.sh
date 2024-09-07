386ceffba07d81cc559e34b86ee84e76b775ebc9
ischeonys <133719190+ischeonys@users.noreply.github.com>
2024-09-07 23:48:16 +0900

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
+
 BUILD_JAR=$(ls /home/ec2-user/build/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
+echo "## Build 파일명: $JAR_NAME"
 
-echo "> build 파일 복사" >> /home/ec2-user/deploy.log
-DEPLOY_PATH=/home/ec2-user/
+DEPLOY_PATH=/home/ec2-user/app/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "## 현재 실행중인 애플리케이션 PID: $CURRENT_PID"
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+  echo "## 현재 구동중인 애플리케이션 없음"
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "## 애플리케이션 종료: kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+echo "## 배포할 JAR 파일: $DEPLOY_JAR"
+nohup java -jar $DEPLOY_JAR > $DEPLOY_PATH/nohup.out 2>&1 &
