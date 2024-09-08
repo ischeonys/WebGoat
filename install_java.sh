012a2035d02350940fd117b485c012069636f029
ischeonys <133719190+ischeonys@users.noreply.github.com>
2024-09-08 18:39:29 +0900

    Delete install_java.sh

--- install_java.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y wget
-wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
-sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
-sudo apt-get update
-sudo apt-get install -y java-17-amazon-corretto-jdk
